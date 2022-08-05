a97a8faf4e0e85b33805c44abf151e369df62207
tigerFish29 <paulwchabz28@gmail.com>
2022-08-05 20:21:11 +0100

    get user method was created

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Code in Java and Spring boot for the purpose of learning and creating simple objects 
 
-My aim is to keep the code for future refrence. 
+My aim is to keep the code for future reference. 
 
 This simple code base simply creates a user to the database.
 
---
+++ src/main/java/com/spring/springuser/Service/UserService.java
@@ -0,0 +1,29 @@
+package com.spring.springuser.Service;
+
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.spring.springuser.Repository.UserRepository;
+import com.spring.springuser.model.UserInfo;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // get a user 
+    public UserInfo getUser(Integer id) {
+        try {
+            return userRepository.findByIdAndActive(id, true).orElseThrow(NotFoundException::new);
+        } catch (NotFoundException e) {
+            
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+}
